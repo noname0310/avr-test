882d6d9af93abc58c97d3f30d1bff0d8374bc48c
noname <48761044+noname0310@users.noreply.github.com>
2021-05-07 00:50:03 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,20 @@ fn main() -> ! {
 
     let mut pins = arduino_uno::Pins::new(dp.PORTB, dp.PORTC, dp.PORTD);
 
-    let mut serial = arduino_uno::Serial::new(
-        dp.USART0,
-        pins.d0,
-        pins.d1.into_output(&mut pins.ddr),
-        9600.into_baudrate(),
-    );
+    // Digital pin 13 is also connected to an onboard LED marked "L"
+    let mut led = pins.d13.into_output(&mut pins.ddr);
 
-    ufmt::uwriteln!(&mut serial, "Hello from Arduino!\r").void_unwrap();
+    led.set_high().void_unwrap();
 
     loop {
-        // Read a byte from the serial connection
-        let b = nb::block!(serial.read()).void_unwrap();
-
-        // Answer
-        ufmt::uwriteln!(&mut serial, "Got {}!\r", b).void_unwrap();
+        
+        led.toggle().void_unwrap();
+        arduino_uno::delay_ms(200);
+        led.toggle().void_unwrap();
+        arduino_uno::delay_ms(200);
+        led.toggle().void_unwrap();
+        arduino_uno::delay_ms(200);
+        led.toggle().void_unwrap();
+        arduino_uno::delay_ms(800);
     }
 }
